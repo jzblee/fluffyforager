283675be7b61ff8a1ca5a0a4d6ab77bbe9ddd7e1
Jason <jason@mutantur.net>
2015-08-12 19:18:11 +0800

    Fine-tune function visibility (and fix commit message grammar)

--- src/factorization.h
+++ src/factorization.h
@@ -6,6 +6,7 @@ using namespace std;
 class factorization {
 public:
   static vector<array<long, 2> > prime_factorize(long factor_this);
-  static long smallest_factor(long operand);
   static vector<long> deduce_divisors(long operand);
+private:
+  static long smallest_factor(long operand);
 };
